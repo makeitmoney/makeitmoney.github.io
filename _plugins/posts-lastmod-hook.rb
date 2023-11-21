a72cfcbcacf654ce8e345bafc52aa1ff27137907
makeitreal <makeitreal119@gmail.com>
2023-11-21 13:03:35 +0900

    avatar,bottom,bg change

--- _plugins/posts-lastmod-hook.rb
+++ _plugins/posts-lastmod-hook.rb
@@ -2,13 +2,27 @@
 #
 # Check for changed posts
 
+# Jekyll::Hooks.register :posts, :post_init do |post|
+
+#   commit_num = `git rev-list --count HEAD "#{ post.path }"`
+
+#   if commit_num.to_i > 1
+#     lastmod_date = `git log -1 --pretty="%ad" --date=iso "#{ post.path }"`
+#     post.data['last_modified_at'] = lastmod_date
+#   end
+
+# end
+
 Jekyll::Hooks.register :posts, :post_init do |post|
-
-  commit_num = `git rev-list --count HEAD "#{ post.path }"`
-
-  if commit_num.to_i > 1
-    lastmod_date = `git log -1 --pretty="%ad" --date=iso "#{ post.path }"`
-    post.data['last_modified_at'] = lastmod_date
+  begin
+    commit_num = `git rev-list --count HEAD "#{ post.path }"`
+    
+    if commit_num.to_i > 1
+      lastmod_date = `git log -1 --pretty="%ad" --date=iso "#{ post.path }"`
+      post.data['last_modified_at'] = lastmod_date
+    end
+  rescue => e
+    puts "Error in posts-lastmod-hook.rb: #{e.message}"
+    puts e.backtrace.join("\n")
   end
-
 end
